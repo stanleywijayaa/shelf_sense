e3b03253ff411ce40ef440b9c0c4e90f70501551
stanleywijayaa <stanleytan2018@gmail.com>
2026-07-07 03:15:56 +0800

    set mainscreen as home,

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/home/main_screen.dart';
 
-/// screens
-import './screens/inventory/add_item_screen.dart';
-import './screens/inventory/inventory_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
-  runApp(const MyApp());
+  runApp(const ShelfSenseApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ShelfSenseApp extends StatelessWidget {
+  const ShelfSenseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      home: const InventoryScreen(), 
+      title: 'ShelfSense',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF3A7D44),
+        ),
+        fontFamily: 'Roboto',
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF8F9FA),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF3A7D44),
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      home: const MainScreen(),
     );
   }
 }
+
